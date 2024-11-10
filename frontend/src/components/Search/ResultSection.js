cf298065ed1c086c4442549cb5439607c3d881c0
Stephen Ng <sng1@ufl.edu>
2024-11-10 16:46:35 -0500

    Fixed login background image and createPost home page link

--- frontend/src/components/Search/ResultSection.js
+++ frontend/src/components/Search/ResultSection.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import SubleaseCard from './SubleaseCard'; // Ensure the path is correct for your project structure
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 const ResultSection = ({ results }) => {
   return (
-    <div>
+    <div style={{ position: 'relative', height: '750px', overflowY: 'auto' }}> {/* Add relative positioning to this container */}
       {results.length === 0 ? (
-        <p>No results found</p>
-      ) : (
-        <div style={{ height: '750px', overflowY: 'auto' }}> {/* Set a fixed height and enable scrolling */}
-          <Grid container spacing={0.5} justifyContent="center">
-            {results.map((listing) => (
-              <Grid item key={listing.id}>
-                <SubleaseCard
-                  images={listing.imageUrls}    
-                  price={listing.rent}        
-                  rooms={listing.numBedrooms}     
-                  bathrooms={listing.numBathrooms} 
-                  sqft={listing.sqft}           
-                  address={listing.address}  
-                />
-              </Grid>
-            ))}
-          </Grid>
+        <div 
+          style={{
+            position: 'absolute',  // Absolute positioning within the parent container
+            top: '20px',           // 10px from the top
+            left: '20px',          // 10px from the left
+            textAlign: 'center',
+            zIndex: 1,             // Ensure it's on top of other content
+          }}
+        >
+          <Typography>No results found...</Typography>
         </div>
+      ) : (
+        <Grid container spacing={0.5} justifyContent="center">
+          {results.map((listing) => (
+            <Grid item key={listing.id}>
+              <SubleaseCard
+                images={listing.imageUrls}    
+                price={listing.rent}        
+                rooms={listing.numBedrooms}     
+                bathrooms={listing.numBathrooms} 
+                sqft={listing.sqft}           
+                address={listing.address}  
+              />
+            </Grid>
+          ))}
+        </Grid>
       )}
     </div>
   );
